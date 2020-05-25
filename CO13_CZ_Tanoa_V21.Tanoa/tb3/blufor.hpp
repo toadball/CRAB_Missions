a803ea40c67b0361b03b3e97670eab9807bb87a2
Andrew Somerville <asmsandrew@googlemail.com>
2020-05-25 02:27:03 +0100

    Updating mission standard loadouts
    Added GPMG class and added cross loaded ammo
    Added MAT and MATA classes for Medium AT capability
    Restructured to be easier to maintain 2 nearly identical gear sets

--- CO13_CZ_Tanoa_V21.Tanoa/tb3/blufor.hpp
+++ CO13_CZ_Tanoa_V21.Tanoa/tb3/blufor.hpp
@@ -1,6 +1,6 @@
 class BLUFOR_MTP {
   class BaseUnit {
-    allowPlayerGoggles = 1;
+    allowPlayerGoggles = 0;
     ace_medic = 1;
     weapons[] = {
       {
@@ -27,18 +27,18 @@ class BLUFOR_MTP {
       "G_Combat_Goggles_tna_F",
       "G_Tactical_Clear"
     };
-
     uniform[] = {"rhs_uniform_g3_mc"};
+
       uniformContents[] = {
         {"ACE_fieldDressing",5},
-				{"ACE_packingBandage",5},
-				{"ACE_quikclot",5},
-				{"ACE_Tourniquet",2},
-				{"ACE_splint",1},
-				{"ACE_morphine",1},
+        {"ACE_packingBandage",5},
+        {"ACE_quikclot",5},
+        {"ACE_Tourniquet",2},
+        {"ACE_splint",1},
+        {"ACE_morphine",1},
         {"ACE_epinephrine",1},
         {"ACE_adenosine",1},
-				{"ACE_salineIV_500",1},
+        {"ACE_salineIV_500",1},
         {"ACE_Flashlight_MX991",1},
         {"ACRE_PRC343",1}
       };
@@ -59,13 +59,14 @@ class BLUFOR_MTP {
 
     backpack[] = {"rhsusf_assault_eagleaiii_coy"};
       backpackContents[] = {
-        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",6},
+        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
         {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
         {"rhsusf_mag_17Rnd_9x19_JHP",2},
         {"HandGrenade",2},
         {"SmokeShell",2},
         {"Chemlight_green",2},
-        {"B_IR_Grenade",1}
+        {"B_IR_Grenade",1},
+        {"rhsusf_100Rnd_762x51_m62_tracer",1}
       };
 
     magazines[] = {}; items[] = {};
@@ -82,7 +83,7 @@ class BLUFOR_MTP {
           }
       },
       {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}},
-      "RangeFinder"
+      "ACE_Vector"
     };
     assignedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemGPS","rhsusf_ANPVS_15"};
 
@@ -98,13 +99,72 @@ class BLUFOR_MTP {
       {"ACRE_PRC148",1},
       {"rhsusf_acc_nt4_black",1},
       {"itc_land_tablet_rover",1},
-      {"ACE_MapTools",1}
+      {"ACE_MapTools",1},
+      {"ACE_MicroDagr",1}
     };
   };
   class ZEUS: PL {
     assignedItems[] = {"ItemMap","ItemCompass","ItemWatch","B_UavTerminal","rhsusf_ANPVS_15"};
   };
   class TL: PL {};
+  class FSO: PL {
+    vestContents[] = {
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
+      {"rhsusf_mag_17Rnd_9x19_JHP",2},
+      {"HandGrenade",2},
+      {"SmokeShell",2},
+      {"Chemlight_green",2},
+      {"B_IR_Grenade",1},
+      {"ace_IR_strobe_item",1},
+      {"ACRE_PRC148",1},
+      {"rhsusf_acc_nt4_black",1},
+      {"ACE_MapTools",1},
+      {"ACE_MicroDagr",1}
+    };
+    backpack[] = {"ITC_Land_o_uav_backpack"};
+    assignedItems[] = {"ItemMap","ItemCompass","ItemWatch","B_UavTerminal","rhsusf_ANPVS_15"};
+      backpackContents[] = {
+        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
+        {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
+        {"HandGrenade",2},
+        {"SmokeShell",2},
+        {"Chemlight_green",2},
+        {"ace_IR_strobe_item",1},
+        {"itc_land_b_ar2i_packed",1},
+        {"ace_uavbattery",2},
+        {"Laserdesignator_01_khk_F",1},
+        {"Laserbatteries",3};
+      };
+
+  };
+  class MED: BaseUnit {
+    ace_medic = 2;
+    vest[] = {"rhsusf_plateframe_medic"};
+    backpack[] = {"B_Carryall_cbr"};
+      backpackContents[] = {
+        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
+        {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
+        {"HandGrenade",2},
+        {"SmokeShell",2},
+        {"Chemlight_green",2},
+        {"B_IR_Grenade",1},
+        {"ACE_fieldDressing",20},
+        {"ACE_packingBandage",20},
+        {"ACE_quikclot",10},
+        {"ACE_elasticBandage",10},
+        {"ACE_morphine",10},
+        {"ACE_epinephrine",10},
+        {"ACE_adenosine",10},
+        {"ACE_salineIV_250",8},
+        {"ACE_salineIV_500",4},
+        {"ACE_bloodIV",1},
+        {"ACE_personalAidKit",1},
+        {"ACE_surgicalKit",1},
+        {"ACE_splint",8},
+        {"ACE_tourniquet",5}
+      };
+  };
   class RF: BaseUnit {};
   class UGL: BaseUnit {
     weapons[] = {
@@ -143,65 +203,10 @@ class BLUFOR_MTP {
         {"1Rnd_Smoke_Grenade_Shell",2}
       };
   };
-  class UAV: BaseUnit {
-    vestContents[] = {
-      {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
-      {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
-      {"rhsusf_mag_17Rnd_9x19_JHP",2},
-      {"HandGrenade",2},
-      {"SmokeShell",2},
-      {"Chemlight_green",2},
-      {"B_IR_Grenade",1},
-      {"ace_IR_strobe_item",1},
-      {"ACRE_PRC148",1},
-      {"rhsusf_acc_nt4_black",1},
-      {"ACE_MapTools",1}
-    };
-    backpack[] = {"itc_land_b_uav_backpack"};
-    assignedItems[] = {"ItemMap","ItemCompass","ItemWatch","B_UavTerminal","rhsusf_ANPVS_15"};
-      backpackContents[] = {
-        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
-        {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
-        {"HandGrenade",2},
-        {"SmokeShell",2},
-        {"Chemlight_green",2},
-        {"ace_IR_strobe_item",1},
-        {"itc_land_b_ar2i_packed",2},
-        {"ace_uavbattery",2}
-      };
-  };
-	class FSO: UAV {};
-  class MED: BaseUnit {
-    ace_medic = 2;
-    vest[] = {"rhsusf_plateframe_medic"};
-    backpack[] = {"B_Carryall_cbr"};
-      backpackContents[] = {
-        {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
-        {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
-        {"HandGrenade",2},
-        {"SmokeShell",2},
-        {"Chemlight_green",2},
-        {"B_IR_Grenade",1},
-        {"ACE_fieldDressing",20},
-        {"ACE_packingBandage",20},
-        {"ACE_quikclot",10},
-        {"ACE_elasticBandage",10},
-        {"ACE_morphine",10},
-        {"ACE_epinephrine",10},
-        {"ACE_adenosine",10},
-        {"ACE_salineIV_250",8},
-        {"ACE_salineIV_500",4},
-        {"ACE_personalAidKit",1},
-        {"ACE_surgicalKit",1},
-        {"ACE_splint",8},
-        {"ACE_tourniquet",5}
-      };
-  };
-
   class LMG: BaseUnit {
     weapons[] = {
       {"rhs_weap_m249_pip_L",
-          {"rhsusf_acc_su230","rhsusf_acc_anpeq15_bk_light","rhsusf_200Rnd_556x45_mixed_soft_pouch_coyote"}
+          {"rhsusf_acc_su230","rhsusf_acc_anpeq15side_bk","rhsusf_200Rnd_556x45_mixed_soft_pouch_coyote"}
       },
       {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}}
     };
@@ -224,10 +229,36 @@ class BLUFOR_MTP {
         {"B_IR_Grenade",1}
       };
   };
+  class GPMG: BaseUnit {
+    weapons[] = {
+      {"rhs_weap_m240G",
+          {"rhsusf_acc_su230a","rhsusf_100Rnd_762x51"}
+      },
+      {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}}
+    };
+    vest[] = {"rhsusf_plateframe_machinegunner"};
+      vestContents[] = {
+        {"rhsusf_100Rnd_762x51",1},
+        {"rhsusf_100Rnd_762x51_m62_tracer",1},
+        {"rhsusf_mag_17Rnd_9x19_JHP",2},
+        {"HandGrenade",2},
+        {"SmokeShell",2},
+        {"Chemlight_green",2},
+        {"B_IR_Grenade",1},
+        {"ACE_MapTools",1}
+      };
+      backpack[]={"rhsusf_falconii_coy"};
+      backpackContents[] = {
+        {"HandGrenade",2},
+        {"SmokeShell",2},
+        {"Chemlight_green",2},
+        {"B_IR_Grenade",1}
+      };
+  };
   class DMR: BaseUnit {
     weapons[] = {
       {"rhs_weap_sr25_ec",
-        {"rhsusf_acc_su230a_mrds_3d","rhsusf_acc_anpeq15side_bk","rhsusf_20Rnd_762x51_SR25_m62_Mag"}
+        {"rhsusf_acc_su230a","rhsusf_acc_anpeq15side_bk","rhsusf_20Rnd_762x51_SR25_m62_Mag"}
       },
       {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}},
       "RangeFinder"
@@ -253,13 +284,51 @@ class BLUFOR_MTP {
         {"B_IR_Grenade",1}
       };
   };
+  class MAT: BaseUnit {
+    weapons[] = {
+      {
+        {"rhs_weap_hk416d145",
+          {"rhsusf_acc_anpeq15_bk_light","rhsusf_acc_su230","rhs_mag_30Rnd_556x45_M855A1_PMAG"}
+        },
+        {"rhs_weap_hk416d10",
+          {"rhsusf_acc_anpeq15_bk_light","rhsusf_acc_su230","rhs_mag_30Rnd_556x45_M855A1_PMAG"}
+        }
+      },
+      {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}},
+      {"launch_MRAWS_green_rail_F",{"MRAWS_HEAT55_F"}}
+    };
 
-	class PILOT: baseUnit {
-		weapons[] = {
+    backpackContents[] = {
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
+      {"rhsusf_mag_17Rnd_9x19_JHP",2},
+      {"HandGrenade",2},
+      {"SmokeShell",2},
+      {"Chemlight_green",2},
+      {"B_IR_Grenade",1},
+      {"MRAWS_HEAT55_F",1},
+      {"MRAWS_HE_F",1}
+    };
+  };
+  class MATA: BaseUnit {
+    backpackContents[] = {
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG",4},
+      {"rhs_mag_30Rnd_556x45_M855A1_PMAG_Tracer_Red",2},
+      {"rhsusf_mag_17Rnd_9x19_JHP",2},
+      {"HandGrenade",2},
+      {"SmokeShell",2},
+      {"Chemlight_green",2},
+      {"B_IR_Grenade",1},
+      {"MRAWS_HEAT55_F",1},
+      {"MRAWS_HE_F",1}
+    };
+  };
+  class PILOT: baseUnit {
+    weapons[] = {
       {"rhsusf_weap_MP7A2",{"rhsusf_mag_40Rnd_46x30_FMJ","rhsusf_acc_eotech_552_d","rhsusf_acc_anpeq15side_bk"}},
       {"rhsusf_weap_glock17g4",{"rhsusf_mag_17Rnd_9x19_JHP"}}
     };
-		headgear[] = {
+    headgear[] = {
       "rhsusf_hgu56p_black",
       "rhsusf_hgu56p_visor_black",
       "rhsusf_hgu56p_mask_black",
@@ -280,26 +349,27 @@ class BLUFOR_MTP {
       "rhsusf_hgu56p_visor_pink",
       "rhsusf_hgu56p_mask_smiley",
       "rhsusf_hgu56p_visor_mask_smiley"
-		};
-		uniform[] = {
-			"rhs_uniform_g3_tan",
-		};
-		vest[] = {
-			"rhsusf_mbav_light",
-		};
-		vestContents[] = {
-			{"rhsusf_mag_40Rnd_46x30_FMJ",6},
-			{"SmokeShellRed",2},
-			{"ACRE_PRC148",1},
-      {"ACE_MapTools",1}
-		};
-		backpack[] = {
-			"rhsusf_falconii_coy","rhsusf_falconii_mc"
-		};
-		backpackContents[] = {
-			{"ToolKit",1}
-		};
-	};
+    };
+    uniform[] = {
+      "rhs_uniform_g3_tan",
+    };
+    vest[] = {
+      "rhsusf_mbav_light",
+    };
+    vestContents[] = {
+      {"rhsusf_mag_40Rnd_46x30_FMJ",6},
+      {"SmokeShellRed",2},
+      {"ACRE_PRC148",1},
+      {"ACE_MapTools",1},
+      {"ACE_MicroDagr",1}
+    };
+    backpack[] = {
+      "rhsusf_falconii_coy","rhsusf_falconii_mc"
+    };
+    backpackContents[] = {
+      {"ToolKit",1}
+    };
+  };
   class LauncherCrate {
     vehCargoWeapons[] = {
       {"rhs_weap_m72a7",10},
@@ -361,6 +431,7 @@ class BLUFOR_MTP {
           {"ACE_adenosine",10},
           {"ACE_salineIV_250",8},
           {"ACE_salineIV_500",4},
+          {"ACE_bloodIV",1},
           {"ACE_personalAidKit",1},
           {"ACE_surgicalKit",1},
           {"ACE_splint",8},
@@ -378,4 +449,60 @@ class BLUFOR_MTP {
       {"rhsusf_assault_eagleaiii_coy",4}
     };
   };
+};
+class BLUFOR_WDL: BLUFOR_MTP {
+
+  class PL: PL {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class ZEUS: ZEUS {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class TL: TL {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class FSO: FSO {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class MED: MED {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class RF: RF {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class UGL: UGL {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class LMG: LMG {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class GPMG: GPMG {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class DMR: DMR {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class MAT: MAT {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class MATA: MATA {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+  class PILOT: PILOT {
+    headgear[] = { "rhsusf_opscore_fg_pelt" };
+    uniform[] = {"rhs_uniform_g3_m81"};
+  };
+
 };
--- CO13_CZ_Tanoa_V21.Tanoa/tb3/loadouts.hpp
+++ CO13_CZ_Tanoa_V21.Tanoa/tb3/loadouts.hpp
@@ -1,8 +1,7 @@
 //Loadouts called with: [this,"side_class","unit_class"] call tb3_fnc_Loadout/tb3_gear_fnc_Loadout;
 //Use those bellow as an example as to creating a side and unit class.
 class TB3_Gear {
-    #include "blufor_mtp.hpp"
-    #include "blufor_wdl.hpp"
+    #include "blufor.hpp"
     #include "napa.hpp"
     #include "ccesf_arid.hpp"
     #include "rhscon_wdl.hpp"
