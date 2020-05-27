c017bcc90f7c92da54e9fe3f3af40ebbd9eeda68
Andrew Somerville <asmsandrew@googlemail.com>
2020-05-27 21:53:01 +0100

    V22 Build

---
+++ CO13_CZ_Malden_V22.Malden/tb3/loadouts.hpp
@@ -0,0 +1,13 @@
+//Loadouts called with: [this,"side_class","unit_class"] call tb3_fnc_Loadout/tb3_gear_fnc_Loadout;
+//Use those bellow as an example as to creating a side and unit class.
+class TB3_Gear {
+    #include "blufor.hpp"
+    #include "napa.hpp"
+    #include "ccesf.hpp"
+    #include "rhscon_wdl.hpp"
+    #include "cdf_wdl.hpp"
+	  #include "brh.hpp"
+    #include "rhs_tnla.hpp"
+    #include "redfor_spetz.hpp"
+    #include "ldf.hpp"
+};
