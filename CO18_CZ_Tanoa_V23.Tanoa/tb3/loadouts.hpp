3dfdc7cf7c9fa4efd4d12bbdc810aacbe59f5ad3
Andrew Somerville <asmsandrew@googlemail.com>
2020-06-10 23:29:14 +0100

    Reorg - Tanoa

--- CO18_CZ_Tanoa_V23.Tanoa/tb3/loadouts.hpp
+++ CO18_CZ_Tanoa_V23.Tanoa/tb3/loadouts.hpp
@@ -1,17 +1,11 @@
 //Loadouts called with: [this,"side_class","unit_class"] call tb3_fnc_Loadout/tb3_gear_fnc_Loadout;
 //Use those bellow as an example as to creating a side and unit class.
 class TB3_Gear {
+    #include "toadball\loadouts.hpp"
     #include "blufor.hpp"
-    #include "blufor_CN.hpp"
-    #include "blufor_saq.hpp"
-    #include "blufor_saqcn.hpp"
-    #include "napa.hpp"
     #include "ccesf.hpp"
-    #include "rhscon_wdl.hpp"
     #include "cdf_wdl.hpp"
-    #include "rhs_tnla.hpp"
   	#include "brh.hpp"
-    #include "redfor_spetz.hpp"
     #include "ldf.hpp"
 	  #include "cecm.hpp"
 	  #include "brit_mtp.hpp"
